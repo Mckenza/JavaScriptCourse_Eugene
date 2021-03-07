b5531cdc640cd1e394ef98caa017684c6ba10a42
Mckenza <Zhenya6207@gmail.com>
2021-03-07 20:06:49 +0300

    pow 3 - task7

--- task7/math.js
+++ task7/math.js
@@ -3,12 +3,12 @@ const array = [4, 2, 5, 19, 13, 0, 10];
 let result = 0;
 
 for(let i = 0; i < array.length; i++){
-    result += Math.pow(array[i],2);
+    result += Math.pow(array[i],3);
 }
 
 result = Math.sqrt(result);
 
-console.log(result); //25.9807....
+console.log(result); // 101.25709....
 
 
 
