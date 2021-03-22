7bc0b3d3e3cb03335c07e34496462857bf8a503e
Mckenza <Zhenya6207@gmail.com>
2021-03-22 21:50:55 +0300

    array add task9

--- Homework_2/task_9/array2d.js
+++ Homework_2/task_9/array2d.js
@@ -5,3 +5,12 @@ const array = [
 
 console.log(array[0][0]);           // Понедельник
 console.log(array[1][2]);           // Wednesday
+
+
+const array2 = [
+    {en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']},
+    {ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота','Воскресенье']}
+]
+
+console.log(array2[0].en[2]);       // Wednesday
+console.log(array2[1].ru[0]);       // Понедельник
