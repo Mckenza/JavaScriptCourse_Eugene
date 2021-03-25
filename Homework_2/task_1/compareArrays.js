f2c8d37425a29f1ed7f11e17a50ac00bfb87af26
Mckenza <Zhenya6207@gmail.com>
2021-03-25 21:29:38 +0300

    edit task

--- Homework_2/task_1/compareArrays.js
+++ Homework_2/task_1/compareArrays.js
@@ -1,31 +1,52 @@
-const a = [1,2,3,4];
+function fillObjMap(array){
+    const obj = {};
+    array.forEach((num)=>{
+        if(num in obj){
+            obj[num]++;
+        } else {
+            obj[num] = 1;
+        }
+    });
+
+    return obj;
+}
+
+const a = [4,2,3,1];
 const b = [1,2,3,4];
 const c = [1,2,3,4,5,6];
 const d = [1,2,3,4,5,6];
 const e = [3,4,5,7,4];
 const f = [1,2,3,5];
-const g = [1,2,3,4,'5',6];
 
 function compare(firstArray, secondArray){
     if(firstArray.length === secondArray.length){
-        let isEqual = true;
-        for(let i = 0; i < firstArray.length; i++){
-            if(firstArray[i] !== secondArray[i]){
-                isEqual = false;
-                console.log('Массивы неравны');
-                console.log(`${firstArray} и ${secondArray}`);
-                break;
+
+        const mapArrayOne = fillObjMap(firstArray);
+        const mapArrayTwo = fillObjMap(secondArray);
+
+        for(let key in mapArrayOne){
+            if(key in mapArrayTwo){
+                if(mapArrayOne[key] !== mapArrayTwo[key]){
+                    console.log('Не равны массивы:');
+                    console.log(`${firstArray}; ${secondArray}`);
+                    return;
+                }
+            } else {
+                console.log('Не равны массивы:');
+                console.log(`${firstArray}; ${secondArray}`);
+                return;
             }
         }
-        if(isEqual){
-            console.log('Равны массивы:')
-            console.log(`${firstArray} и ${secondArray}`);
-        }
+        console.log('Равные массивы:');
+        console.log(`${firstArray}; ${secondArray}`);
+
+
     } else {
-        console.log('Неравны массивы:');
+        console.log('Не равны массивы:');
         console.log(`${firstArray} и ${secondArray}`);
     }
 }
 
-compare(a,b);               // Равны
-compare(a,c);               // Неравны
+compare(a,b);        
+compare(a,c);
+
